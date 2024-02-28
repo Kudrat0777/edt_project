3d9084a4682c30b0e6c66c278758dd56df1f1d94
Kudrat0777 <114284502+AnnSkachkova@users.noreply.github.com>
2024-02-28 19:00:10 +0300

    юрлы определены

--- apps/bot_app/urls.py
+++ apps/bot_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from .views import complaint_list, submit_complaint, respond_to_complaint
+
 
 app_name = 'bot_app'
 
+
 urlpatterns = [
-    
+    path('complaints/', complaint_list, name='complaint_list'),
+    path('submit_complaint/', submit_complaint, name='submit_complaint'),
+    path('respond_to_complaint/<int:complaint_id>/', respond_to_complaint, name='respond_to_complaint'),
 ]
