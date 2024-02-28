83383f74c229127ccb9f51c68476cd96673922d5
Kudrat0777 <114284502+AnnSkachkova@users.noreply.github.com>
2024-02-28 18:57:24 +0300

    представления определены

--- apps/bot_app/views.py
+++ apps/bot_app/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import CivilComplaint, EmployeeResponse
+from .forms import CivilComplaintForm, EmployeeResponseForm
 
-# Create your views here.
+def complaint_list(request):
+    complaints = CivilComplaint.objects.all()
+    return render(request, 'complaint_list.html', {'complaints': complaints})
+
+def submit_complaint(request):
+    if request.method == 'POST':
+        form = CivilComplaintForm(request.POST)
+        if form.is_valid():
+            complaint = form.save(commit=False)
+            complaint.user = request.user  # Привязываем обращение к текущему пользователю
+            complaint.save()
+            return redirect('complaint_list')
+    else:
+        form = CivilComplaintForm()
+
+    return render(request, 'submit_complaint.html', {'form': form})
+
+def respond_to_complaint(request, complaint_id):
+    complaint = CivilComplaint.objects.get(pk=complaint_id)
+
+    if request.method == 'POST':
+        form = EmployeeResponseForm(request.POST)
+        if form.is_valid():
+            response = form.save(commit=False)
+            response.complaint = complaint
+            response.save()
+
+            complaint.is_resolved = True
+            complaint.save()
+
+            return redirect('complaint_list')
+    else:
+        form = EmployeeResponseForm()
+
+    return render(request, 'respond_to_complaint.html', {'form': form, 'complaint': complaint})
